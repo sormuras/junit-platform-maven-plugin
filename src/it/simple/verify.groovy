3e208813d94d02b80f96039b4ebdc9e266182834
Christian Stein <sormuras@gmail.com>
2018-08-17 06:38:45 +0200

    Avoid Groovy as scripting language
    Too many illegal reflective access operations

--- src/it/simple/verify.groovy
+++
@@ -1,8 +0,0 @@
-assert new File( basedir, "target/test-classes/JupiterTest.class" ).exists()
-assert new File( basedir, "target/test-classes/JupiterTests.class" ).exists()
-assert new File( basedir, "target/test-classes/TestJupiter.class" ).exists()
-
-def log = new File( basedir, 'build.log' ).getText('UTF-8')
-
-assert log.contains( '[INFO] Launching JUnit Platform...' )
-assert log.contains( '[INFO] Successfully executed: 3 test(s)' )
