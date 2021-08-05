c06c0b76ce3380e1e38903c39e634971d2d85035
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-08-05 16:09:03 +0300

    test: added log match verfication to the integration test

--- src/it/modular-multi-modules-no-tests/verify.groovy
+++ src/it/modular-multi-modules-no-tests/verify.groovy
@@ -4,6 +4,12 @@ def verifier = new Verifier(basedir.toPath())
 
 verifier.verifyBadLines()
 
-// TODO add verifier.verifyLogMatches invocation
+verifier.verifyLogMatches ">> BEGIN >>",
+  "    <failIfNoTests>false</failIfNoTests>",
+  ">> A bunch of log lines... >>",
+  "\\Q[DEBUG] Patched directory \\E.*/module-with-only-sources/target/junit-platform/patched-test-runtime was not found and failIfNoTests is set to false",
+  ">> A bunch of log lines... >>",
+  "[INFO] BUILD SUCCESS",
+  ">> END. >>"
 
 verifier.isOk()
