c182bacc0e2b6b14b638057d68a206c412036462
Tomer Figenblat <tomer.figenblat@gmail.com>
2021-08-09 17:09:13 +0300

    fix: elements from the test classpath should be placed within the testpathes set

--- src/main/java/de/sormuras/junit/platform/maven/plugin/MavenDriver.java
+++ src/main/java/de/sormuras/junit/platform/maven/plugin/MavenDriver.java
@@ -108,7 +108,7 @@ class MavenDriver implements Driver {
 
     // Acquire all main and test path elements from the project...
     try {
-      addAll(project.getTestClasspathElements(), mainPaths);
+      addAll(project.getTestClasspathElements(), testPaths);
 
       // Exclude all compile elements from test paths per default
       Set<String> excludePaths = new LinkedHashSet<>(project.getCompileClasspathElements());
