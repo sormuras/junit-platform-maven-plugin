2c5de3be00202084b42edf2fd9faa1504b05a7fb
Christian Stein <sormuras@gmail.com>
2021-08-21 06:29:57 +0200

    Make expected log line platform-agnostic

--- src/it/modular-multi-modules-no-tests/verify.groovy
+++ src/it/modular-multi-modules-no-tests/verify.groovy
@@ -7,7 +7,7 @@ verifier.verifyBadLines()
 verifier.verifyLogMatches ">> BEGIN >>",
   "    <failIfNoTests>false</failIfNoTests>",
   ">> A bunch of log lines... >>",
-  "\\Q[DEBUG] Patched directory \\E.*/module-with-only-sources/target/junit-platform/patched-test-runtime was not found and failIfNoTests is set to false",
+  "\\Q[DEBUG] Patched directory \\E.*module-with-only-sources.target.junit-platform.patched-test-runtime was not found and failIfNoTests is set to false",
   ">> A bunch of log lines... >>",
   "[INFO] BUILD SUCCESS",
   ">> END. >>"
