fef6032a059412c3251fe613cd712d1ef751aae1
Daniel Widdis <widdis@gmail.com>
2023-12-08 12:35:44 -0800

    Handle UTF-16 / UTF-32 with leading null byte
    Signed-off-by: Daniel Widdis <widdis@gmail.com>

--- src/it/charset/src/test/java/CharsetTests.java
+++ src/it/charset/src/test/java/CharsetTests.java
@@ -4,6 +4,8 @@ class CharsetTests {
 
   @Test
   void outputSomething() throws Exception {
+    // The degree sign (hex b0) will be prepended by 2c in UTF-8
+    // For ISO output check for optional Â (hex 2c) character
     System.out.println("UTF-8 degree sign is °");
   }
 }
